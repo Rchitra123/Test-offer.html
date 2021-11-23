641d1e2b759ddd9d4d2a33d201d7f9dc9df5f559
Rchitra123 <94674021+Rchitra123@users.noreply.github.com>
2021-11-23 15:41:18 +0530

    Create test-offer.main.php

---
+++ test-offer.main.php
@@ -0,0 +1,5 @@
+document.getElementById("demo").style.fontSize = "25px";
+document.getElementById("demo").style.color = "red";
+document.getElementById("demo").style.backgroundColor = "yellow";
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+document.getElementById("image").src = "picture.gif";
